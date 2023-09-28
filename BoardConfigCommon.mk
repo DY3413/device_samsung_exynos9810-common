7d12af67834b9d1f2d7fdb501dc45da27f21dbe4
DY3413 <milosix3413@gmail.com>
2023-09-28 23:52:20 +0900

    exynos9810-common: Drop vendor security patch level
    Signed-off-by: DY3413 <milosix3413@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -100,9 +100,6 @@ TARGET_RELEASETOOLS_EXTENSIONS := $(COMMON_PATH)/releasetools
 # RIL
 ENABLE_VENDOR_RIL_SERVICE := true
 
-# Security
-VENDOR_SECURITY_PATCH := 2021-11-01
-
 # Sepolicy
 BOARD_SEPOLICY_TEE_FLAVOR := mobicore
 include device/lineage/sepolicy/exynos/sepolicy.mk
