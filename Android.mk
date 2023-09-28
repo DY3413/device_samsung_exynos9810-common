21f049ed2e77c0531a716d3e0f11903c6b5e6355
DY3413 <milosix3413@gmail.com>
2023-09-29 00:26:15 +0900

    exynos9810-common: Clean up makefiles
    Signed-off-by: DY3413 <milosix3413@gmail.com>

--- Android.mk
+++ Android.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2021 The LineageOS Project
+# Copyright (C) 2021-2023 The LineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -8,8 +8,7 @@ LOCAL_PATH := $(call my-dir)
 
 ifneq ($(filter starlte star2lte crownlte, $(TARGET_DEVICE)),)
 
-  subdir_makefiles=$(call first-makefiles-under,$(LOCAL_PATH))
-  $(foreach mk,$(subdir_makefiles),$(info including $(mk) ...)$(eval include $(mk)))
+include $(call all-subdir-makefiles,$(LOCAL_PATH))
 
 include $(CLEAR_VARS)
 
--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2021 The LineageOS Project
+# Copyright (C) 2021-2023 The LineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -9,20 +9,16 @@ COMMON_PATH := device/samsung/exynos9810-common
 BUILD_BROKEN_DUP_RULES := true
 BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
 
-## Include path
+# Include path
 TARGET_SPECIFIC_HEADER_PATH := $(COMMON_PATH)/include
 
-## Inherit proprietary vendor configuration
-include vendor/samsung/exynos9810-common/BoardConfigVendor.mk
-
-## Architecture
+# Architecture
 TARGET_ARCH := arm64
 TARGET_ARCH_VARIANT := armv8-a
 TARGET_CPU_ABI := arm64-v8a
 TARGET_CPU_ABI2 :=
 TARGET_CPU_VARIANT := cortex-a53
 
-## Architecture (secondary)
 TARGET_2ND_ARCH := arm
 TARGET_2ND_ARCH_VARIANT := armv8-a
 TARGET_2ND_CPU_ABI := armeabi-v7a
@@ -35,20 +31,20 @@ BOARD_CUSTOM_BOOTIMG_MK := hardware/samsung/mkbootimg.mk
 BOARD_KERNEL_BASE := 0x10000000
 BOARD_KERNEL_OFFSET := 0x00008000
 BOARD_KERNEL_PAGESIZE := 2048
-BOARD_KERNEL_RAMDISK_OFFSET := 0x01000000
-BOARD_KERNEL_TAGS_OFFSET     := 0x00000100
+BOARD_RAMDISK_OFFSET := 0x01000000
+BOARD_TAGS_OFFSET := 0x00000100
 
-BOARD_MKBOOTIMG_ARGS += --ramdisk_offset $(BOARD_KERNEL_RAMDISK_OFFSET)
-BOARD_MKBOOTIMG_ARGS += --tags_offset $(BOARD_KERNEL_TAGS_OFFSET)
-BOARD_MKBOOTIMG_ARGS := --kernel_offset $(BOARD_KERNEL_OFFSET)
+BOARD_MKBOOTIMG_ARGS += --kernel_offset $(BOARD_KERNEL_OFFSET)
+BOARD_MKBOOTIMG_ARGS += --ramdisk_offset $(BOARD_RAMDISK_OFFSET)
+BOARD_MKBOOTIMG_ARGS += --tags_offset $(BOARD_TAGS_OFFSET)
 
-## Filesystem
+# Filesystem
 BOARD_CACHEIMAGE_FILE_SYSTEM_TYPE := ext4
 BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
 TARGET_COPY_OUT_VENDOR := vendor
 TARGET_USERIMAGES_USE_EXT4 := true
 
-## Fingerprint
+# Fingerprint
 TARGET_SEC_FP_HAS_FINGERPRINT_GESTURES := true
 
 # Kernel
@@ -74,15 +70,14 @@ BOARD_VENDORIMAGE_PARTITION_SIZE := 681574400
 
 BOARD_ROOT_EXTRA_FOLDERS := efs
 
-
-## Platform
+# Platform
 BOARD_VENDOR := samsung
 TARGET_BOARD_PLATFORM := universal9810
-TARGET_SLSI_VARIANT := bsp
 TARGET_SOC := exynos9810
 TARGET_BOOTLOADER_BOARD_NAME := exynos9810
+include hardware/samsung_slsi-linaro/config/BoardConfig9810.mk
 
-## Properties
+# Properties
 TARGET_SYSTEM_PROP += $(COMMON_PATH)/system.prop
 TARGET_PRODUCT_PROP += $(COMMON_PATH)/product.prop
 TARGET_VENDOR_PROP += $(COMMON_PATH)/vendor.prop
@@ -92,9 +87,6 @@ BOARD_HAS_DOWNLOAD_MODE := true
 BOARD_USES_FULL_RECOVERY_IMAGE := true
 TARGET_RECOVERY_PIXEL_FORMAT := "ABGR_8888"
 TARGET_RECOVERY_FSTAB := $(COMMON_PATH)/configs/init/fstab.samsungexynos9810
-
-## Releasetools
-TARGET_RELEASETOOLS_EXTENSIONS := $(COMMON_PATH)/releasetools
 
 # RIL
 ENABLE_VENDOR_RIL_SERVICE := true
@@ -106,10 +98,10 @@ include device/samsung_slsi/sepolicy/sepolicy.mk
 
 BOARD_VENDOR_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/vendor
 
-## VNDK
+# VNDK
 BOARD_VNDK_VERSION := current
 
-## Wi-Fi
+# Wi-Fi
 BOARD_WLAN_DEVICE                := bcmdhd
 BOARD_WPA_SUPPLICANT_DRIVER      := NL80211
 BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_$(BOARD_WLAN_DEVICE)
@@ -118,5 +110,8 @@ BOARD_HOSTAPD_PRIVATE_LIB        := lib_driver_cmd_$(BOARD_WLAN_DEVICE)
 WIFI_AVOID_IFACE_RESET_MAC_CHANGE := true
 WIFI_HIDL_FEATURE_DUAL_INTERFACE := true
 WIFI_HIDL_UNIFIED_SUPPLICANT_SERVICE_RC_ENTRY := true
-WPA_SUPPLICANT_VERSION           := VER_0_8_X
 WIFI_DRIVER_FW_PATH_PARAM        := "/sys/module/dhd/parameters/firmware_path"
+WPA_SUPPLICANT_VERSION           := VER_0_8_X
+
+# Inherit the proprietary files
+include vendor/samsung/exynos9810-common/BoardConfigVendor.mk
